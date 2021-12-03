c64482a481d308a49e6a368e9f60c8c4640d9cce
Richard Baret <richardbaret@gmail.com>
2021-12-03 17:24:27 +0100

    Day 3 complete !

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -12,10 +12,29 @@ namespace Day3
         {
             string path = ".\\input.txt";
             List<string> values = Utility.ImportInput.ToStringList(path);
-            Console.Write("Exercise 1 : "+Exercise1(values));
+            Console.WriteLine("Exercise 1 : " + Exercise1(values));
+            Console.WriteLine("Exercise 2 : " + Exercise2(values));
         }
 
         static int Exercise1(List<string> values)
+        {
+            Tuple<string, string> rates = getGammaEpsilonRates(values);
+            string gammaRate = rates.Item1;
+            string epsilonRate = rates.Item2;
+            return (Convert.ToInt32(gammaRate, 2) * Convert.ToInt32(epsilonRate, 2));
+
+        }
+
+        static int Exercise2(List<string> values)
+        {
+            Tuple<string, string> ratings = getOxygenCO2Scrubberratings(values);
+            string oxGeneratorRating = ratings.Item1;
+            string co2ScrubberRating = ratings.Item2;
+            return (Convert.ToInt32(oxGeneratorRating, 2) * Convert.ToInt32(co2ScrubberRating, 2));
+
+        }
+
+        public static Tuple<string, string> getGammaEpsilonRates(List<string> values)
         {
             int counter = 0;
             int[] bitWeight = Enumerable.Repeat(0, 12).ToArray(); // Array of 12 int set to 0 to store the sum of each bit position
@@ -23,6 +42,7 @@ namespace Day3
             string epsilonRate = "";
             StringBuilder gammaBuilder = new StringBuilder();
             StringBuilder epsilonBuilder = new StringBuilder();
+            Tuple<string, string> rates;
             foreach (string value in values)
             {
                 int index = 0;
@@ -38,7 +58,7 @@ namespace Day3
             }
 
             // Gamma/Epsilon rate calculation
-            // Now let's divide each int by the total number of line. If the result is >0.5 then 1 is predominant, else 0 is
+            // Now let's divide each int by the total number of lines. If the result is >0.5 then 1 is predominant, else 0 is (this is a simple average calculation)
             foreach (int bitOccurences in bitWeight)
             {
                 float predominance = (float)bitOccurences / (float)counter;
@@ -55,8 +75,68 @@ namespace Day3
             }
             gammaRate = gammaBuilder.ToString();
             epsilonRate = epsilonBuilder.ToString();
-            return (Convert.ToInt32(gammaRate, 2) * Convert.ToInt32(epsilonRate, 2));
+            rates = new Tuple<string, string>(gammaRate, epsilonRate);
+            return rates;
+        }
+        public static Tuple<string, string> getOxygenCO2Scrubberratings(List<string> values)
+        {
+            Tuple<string,string> gammaEpsilonRates; // We will reuse the resul of the function from exercise 1 to get the most prominent bit of the remaining items
+            string gammaRate=""; 
+            string epsilonRate="";
+            List<string> currentValuesForOxy = new List<string>(values);
+            List<string> currentValuesForCo2 = new List<string>(values);
+            List<string> valuesToKeepForOxy = new List<string>();
+            List<string> valuesToKeepForCo2 = new List<string>();
+            string oxyRating = "";
+            string co2Rating = "";
+            Tuple<string, string> ratings;
+            for (int bitIndex = 0; bitIndex < 12; bitIndex++)
+            {
+                gammaEpsilonRates = getGammaEpsilonRates(currentValuesForOxy); //Recalculating the most prominent bits of the remaining items in the list at each pass (suboptimal because we calculate for both rates and all bits but we don't care about perf here :o)
+                gammaRate = gammaEpsilonRates.Item1;
+                foreach (string value in currentValuesForOxy)
+                {
 
+                    if (value.ElementAt(bitIndex) == gammaRate.ElementAt(bitIndex))
+                    {
+                        valuesToKeepForOxy.Add(value);                        
+                    }
+                    
+                }
+                if (valuesToKeepForOxy.Count == 1)
+                {
+                    oxyRating = valuesToKeepForOxy.First();
+                    break;
+                }
+                currentValuesForOxy = new List<string>(valuesToKeepForOxy);
+                valuesToKeepForOxy = new List<string>();
+            }
+
+            for (int bitIndex = 0; bitIndex < 12; bitIndex++)
+            {
+                gammaEpsilonRates = getGammaEpsilonRates(currentValuesForCo2);
+                epsilonRate = gammaEpsilonRates.Item2;
+                foreach (string value in currentValuesForCo2)
+                {
+                    if (value.ElementAt(bitIndex) == epsilonRate.ElementAt(bitIndex))
+                    {
+                        valuesToKeepForCo2.Add(value);
+                    }
+                }
+
+                
+                if (valuesToKeepForCo2.Count == 1)
+                {
+                    co2Rating = valuesToKeepForCo2.First();
+                    break;
+                }
+                currentValuesForCo2 = new List<string>(valuesToKeepForCo2);
+                valuesToKeepForCo2 = new List<string>();
+
+
+            }
+            ratings = new Tuple<string, string>(oxyRating, co2Rating);
+            return ratings;
         }
     }
 }
