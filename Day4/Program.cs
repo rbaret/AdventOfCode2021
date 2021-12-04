665ce48bf26a013190512046f29dce7b2a7db49f
Richard Baret <richardbaret@gmail.com>
2021-12-04 18:08:39 +0100

    Day 4 start

---
+++ Day4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using Utility;
+
+namespace Day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
