e60414a63959b09f4f3c34d38843338796a8a7fa
Richard Baret <richard.baret@outlook.fr>
2021-12-04 22:41:39 +0100

    started dev to generate bingo cards

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Linq;
+using System.Collections.Generic;
+using System.Text;
 using Utility;
 
 namespace Day4
@@ -8,7 +10,37 @@ namespace Day4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = ".\\input.txt";
+            List<string> values = Utility.ImportInput.ToStringList(path);
+            int Exercise1 = Bingo(values);
+        }
+
+        public static int Bingo(List<string> values)
+        {
+            string drawnValues = values.First();
+            // We use a list of Dictionaries because this way we can use the number as key and a bool to store the mark status
+            List<Dictionary<int, bool>> bingoCards = generateCards(values.Skip(2).ToList()); // Skip the first two lines to send only the cards content
+            Console.WriteLine(drawnValues);
+            return 1;
+        }
+
+        public static List<Dictionary<int, bool>> generateCards(List<string> values)
+        {
+            List<Dictionary<string, bool>> cards = new List<Dictionary<string, bool>>();
+            Dictionary<string, bool> buffer = new Dictionary<string, bool>();
+            foreach (string row in values)
+            {
+                List<string> rowTiles = row.Split(' ').ToList;
+                foreach (string tile in rowTiles) { }
+                if (string.IsNullOrWhiteSpace)
+                {
+                    cards.Add(new Dictionary<int, bool>(buffer.ToDictionary));
+                    buffer.Clear();
+                    rowNum = 0;
+                    colNum = 0;
+                }
+            }
+            return cards;
         }
     }
 }
