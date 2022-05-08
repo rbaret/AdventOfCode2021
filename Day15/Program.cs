2a6a921516af396dce57f9b1c977b1883e945057
Richard Baret <richardbaret@gmail.com>
2022-05-08 21:30:38 +0200

    Last commit for 2021

---
+++ Day15/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Utility;
+
+namespace Day15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "./input.txt";
+            int[,] input = Utility.ImportInput.ToIntMatrix(path);
+        }
+
+        public static int Exercise1(int[,] input)
+        {
+            List<Tuple<int,int>> sptSet = new List<Tuple<int, int>>();
+            
+            return 0;
+        }
+    }
+}
