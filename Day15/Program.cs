7558d42eadcc8fc783b3161860a3373cfc21dad7
rbaret <richardbaret@gmail.com>
2022-12-18 15:00:17 +0100

    Day15

--- Day15/Program.cs
+++ Day15/Program.cs
@@ -9,11 +9,9 @@ namespace Day15
     {
         public int x;
         public int y;
-        public int f = 0;
-        public int g = 0;
-        public int h = 0;
         public int risk;
-        public Node parent;
+        public int cost = int.MaxValue;
+        public Node parent = null;
         public Node(int x, int y, int risk)
         {
             this.x = x;
@@ -21,15 +19,13 @@ namespace Day15
             this.risk = risk;
         }
 
-        public Node(Node newNode, Node parent)
+        public Node(Node newNode, Node parent, int cost)
         {
             this.x = newNode.x;
             this.y = newNode.y;
             this.risk = newNode.risk;
-            this.f = newNode.f;
-            this.g = newNode.g;
-            this.h = newNode.h;
             this.parent = parent;
+            this.cost = cost;
         }
 
         public Node(Node n)
@@ -37,10 +33,8 @@ namespace Day15
             this.x = n.x;
             this.y = n.y;
             this.risk = n.risk;
-            this.f = n.f;
-            this.g = n.g;
-            this.h = n.h;
             this.parent = n.parent;
+            this.cost = n.cost;
         }
 
         public override bool Equals(object obj)
@@ -55,130 +49,91 @@ namespace Day15
 
         public override int GetHashCode()
         {
-            return x ^ y;
+            return HashCode.Combine(x, y);
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            string path = "input.txt";
-            int[,] input = Utility.ImportInput.ToIntMatrix(path);
-            Part1(input);
+            string path = "test_input.txt";
+            int[,] riskMap = Utility.ImportInput.ToIntMatrix(path);
+            Part1(riskMap);
         }
-        // Create a new type to represent a point in the cave
 
         // Part 1
-        // Use A* to find the shortest path to the destination based on lowest risk probability
+        // Use Dijkstra to find the shortest path to the destination based on lowest risk probability
         // Starting point is (0,0), destination is input[intput.Length-1, input[0].Length-1]
-        public static void Part1(int[,] input)
+        public static void Part1(int[,] riskMap)
+        {
+            Node start = new Node(0, 0, riskMap[0, 0]);
+            Node end = new Node(riskMap.GetLength(0) - 1, riskMap.GetLength(1) - 1, riskMap[riskMap.GetLength(0) - 1, riskMap.GetLength(1) - 1]);
+            List<Node> path = Dijkstra(riskMap, start, end);
+            displayPath(path);
+            Console.WriteLine("Part 1: The path with the lowest risk to the exit has a risk of {0}", path.Select(x => x.risk).Sum());
+        }
+
+        public static List<Node> Dijkstra(int[,] riskMap, Node start, Node end)
         {
             // Create a map of the cave with the risk value for each node
-
-            Node[,] map = new Node[input.GetLength(0), input.GetLength(1)];
-            for (int i = 0; i < input.GetLength(0); i++)
+            List<Node> path = new List<Node>();
+            List<Node> visited = new List<Node>();
+            List<Node> unvisited = new List<Node>();
+            unvisited = initNodeList(riskMap);
+            unvisited.Where(x => x.Equals(start)).First().cost = 0;
+            while (unvisited.Count > 0 && !visited.Contains(end))
             {
-                for (int j = 0; j < input.GetLength(1); j++)
+                // Get the node with the lowest risk
+                Node current = unvisited.OrderBy(x => x.cost).First();
+                unvisited.Remove(current);
+                visited.Add(current);
+                List<Node> neighbors = GetValidNeighbors(riskMap, current);
+                foreach (Node n in neighbors)
                 {
-                    int risk = input[i, j];
-                    map[i, j] = new Node(i, j, risk);
-                }
-            }
-
-            Node start = map[0, 0];
-            Node end = map[input.GetLength(0) - 1, input.GetLength(1) - 1];
-
-            List<Node> openList = new List<Node>();
-            List<Node> closedList = new List<Node>();
-
-            // set g and f scores for start node
-            start.g = start.f = start.h = 0;
-            // add start to open list and strt algorithm
-            openList.Add(start);
-
-            // Crawl through the list of open nodes until we find the end node
-            while (openList.Count > 0 && !closedList.Contains(end))
-            {
-                // find node with lowest f
-                Node q = openList.Select(x => x).OrderBy(x => x.f).First();
-
-                // Pop the node off the open list
-                openList.Remove(q);
-                if (q == end)
-                {
-                    break;
-                }
-
-
-                // for each neighbor of node
-                List<Node> neighbors = GetNeighbors(q, map);
-                foreach (Node neighbor in neighbors)
-                {
-                    // if neighbor is destination, stop search
-                    if (neighbor == end)
+                    if (!visited.Contains(n))
                     {
-                        break;
-                    }
-                    // else update neighbor's g and f
-                    else
-                    {
-                        neighbor.g = q.g + neighbor.risk;
-                        neighbor.h = Heuristic(neighbor.x, neighbor.y, end.x, end.y);
-                        neighbor.f = neighbor.g + neighbor.h;
-                    }
-
-                    // if neighbor is in open list, but with higher g, skip
-                    if (openList.Contains(neighbor))
-                    {
-                        if (openList.Find(x => x.x == neighbor.x && x.y == neighbor.y).g > neighbor.g)
+                        // If the node is already in the unvisited list, update the parent if the risk is lower
+                        int newCost = current.cost + n.risk;
+                        if(newCost < n.cost)
                         {
-                                continue;}
-                    }
-
-                    // else if neighbor is in closed list, but with lower g, skip
-                    else if (closedList.Contains(neighbor))
-                    {
-                        Node n = new Node(closedList.Find(x => x.x == neighbor.x && x.y == neighbor.y));
-                        if (n.g < neighbor.g)
-                        {
-                            continue;
+                            unvisited.Where(x => x.Equals(n)).First().cost = newCost;
+                            unvisited.Where(x => x.Equals(n)).First().parent = current;
                         }
-                        else
-                        {
-                            closedList.Remove(n);
-                            closedList.Add(neighbor);
-                        }
-                    }
-                    // else add neighbor to open list
-                    else
-                    {
-                        openList.Add(neighbor);
-                    }
+                   }
                 }
-                closedList.Add(q);
             }
-
-            // Reconstruction of path
-            Node pathNode = closedList.Last();
-            int pathRisk = 0;
-            while (pathNode.parent != null)
+            Node currentNode = visited.Last();
+            // Reconstruct the path
+            while (currentNode.parent != null)
             {
-                Console.WriteLine("Node: " + pathNode.x + ", " + pathNode.y + " Risk: " + pathNode.risk);
-                pathRisk += pathNode.risk;
-                pathNode = pathNode.parent;
+                path.Add(currentNode);
+                currentNode = currentNode.parent;
             }
-
-            Console.WriteLine("Part 1: The risk of the path to the exit is " + pathRisk);
-
+            path.Reverse();
+            return path;
         }
 
-        // Heuristic calculated using manhattan distance
-        public static int Heuristic(int x1, int y1, int x2, int y2)
+        public static void displayPath(List<Node> path)
         {
-            return Math.Abs(x1 - x2) + Math.Abs(y1 - y2);
+            foreach (Node n in path)
+            {
+                Console.WriteLine("({0},{1}) Risk: {2}", n.x, n.y, n.risk);
+            }
+        }
+        public static List<Node> initNodeList(int[,] riskMap)
+        {
+            List<Node> nodes = new List<Node>();
+            for (int i = 0; i < riskMap.GetLength(0); i++)
+            {
+                for (int j = 0; j < riskMap.GetLength(1); j++)
+                {
+                    nodes.Add(new Node(i, j, riskMap[i, j]));
+                }
+            }
+            return nodes;
         }
 
-        public static List<Node> GetNeighbors(Node n, Node[,] map)
+        public static List<Node> GetValidNeighbors(int[,] riskMap, Node n)
         {
 
             // Return the nighbors for a given node
@@ -186,19 +141,19 @@ namespace Day15
             List<Node> neighbors = new List<Node>();
             if (n.x > 0)
             {
-                neighbors.Add(new Node(map[n.x - 1, n.y], n));
+                neighbors.Add(new Node(n.x - 1, n.y, riskMap[n.x - 1, n.y]));
             }
-            if (n.x < map.GetLength(0) - 1)
+            if (n.x < riskMap.GetLength(0) - 1)
             {
-                neighbors.Add(new Node(map[n.x + 1, n.y], n));
+                neighbors.Add(new Node(n.x + 1, n.y, riskMap[n.x + 1, n.y]));
             }
             if (n.y > 0)
             {
-                neighbors.Add(new Node(map[n.x, n.y - 1], n));
+                neighbors.Add(new Node(n.x, n.y - 1, riskMap[n.x, n.y - 1]));
             }
-            if (n.y < map.GetLength(1) - 1)
+            if (n.y < riskMap.GetLength(1) - 1)
             {
-                neighbors.Add(new Node(map[n.x, n.y + 1], n));
+                neighbors.Add(new Node(n.x, n.y + 1, riskMap[n.x, n.y + 1]));
             }
             return neighbors;
         }
